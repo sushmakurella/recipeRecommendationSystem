cb6145797191253a6b398c9faccc4af3bbd60e01
sushmakurella <suahmakurella@gmail.com>
2023-06-03 16:48:30 +0530

    more changes

--- recipeRecommendation/settings.py
+++ recipeRecommendation/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f4xk!987$m)vgn(ql&xl#^^8!s6w3g_03)-esny6p4=dstrvy^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -120,7 +120,10 @@ import os
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
